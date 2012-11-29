705ac89386f9a4bebfc15a91848e20f1fb78c3f1
ross <ross@yoyogames.com>
2012-11-29 13:51:55 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4a25f53f45346c986711fe748c9a4be7aa4d470cfc9d5773081ae8a45e05e3ae16fa0fe0587caa3cf87e60552b1001792ef22c6c4f85c43ce110a228b93424d1'
